7b0f4e1c1889e8ca391d272722e275c4dfa1a29e
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2025-05-20 07:26:40 +0200

    Added other parsing options

--- src/components/Survey/customPlugin.ts
+++ src/components/Survey/customPlugin.ts
@@ -2,22 +2,46 @@ import { visit } from "unist-util-visit";
 import { Root } from "mdast";
 import type { Literal } from "unist";
 
-function checkQuestion(value: string) {
+const checkQuestion = (value: string) => {
   const match = /^\? *(\w[\w]*) *(.*)/.exec(value);
   console.log(match);
   if (match) {
     return true;
   }
   return false;
-}
+};
+
+const checkCondition = (value: string) => {
+  const match = /^@ *(.*)/.exec(value);
+  if (match) {
+    return true;
+  }
+  return false;
+};
+
+const checkPrint = (value: string) => {
+  const match = /^\{([^}]+)\}/.exec(value);
+  if (match) {
+    return true;
+  }
+  return false;
+};
 
 const questionType = "question";
+const conditionType = "condition";
+const printType = "print";
 
 export default function customPlugin() {
   return (tree: Root) => {
-    visit(tree, "text", (node: Literal) => {
+    visit(tree, "text", (node: any) => {
       if (typeof node.value === "string" && checkQuestion(node.value)) {
         node.type = questionType;
+      }
+      if (typeof node.value === "string" && checkCondition(node.value)) {
+        node.type = conditionType;
+      }
+      if (typeof node.value === "string" && checkPrint(node.value)) {
+        node.type = printType;
       }
     });
   };
