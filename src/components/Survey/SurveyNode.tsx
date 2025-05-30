da547cafba961b89fbf01786a17ad5b415e906b8
Bratislav Ljubisic <bratislav.ljubisic@icloud.com>
2025-05-30 08:44:19 +0200

    Intermitent changes

--- src/components/Survey/SurveyNode.tsx
+++ src/components/Survey/SurveyNode.tsx
@@ -1,9 +1,55 @@
+import { createFunction } from "./expression";
+
 export type NodeProps = {
-  text: string;
+  context: {
+    [x: string]: any;
+  };
+  next: () => {};
+  node: {
+    condition: { expr: any };
+    type: string;
+    value: string;
+    question: any;
+    depth: number;
+    children: any;
+    url: string | string[];
+    alt: any;
+  };
 };
 
-const SurveyNode: React.FC<NodeProps> = ({ text }) => {
-  return <>{text}</>;
+const SurveyNode: React.FC<NodeProps> = ({ node, context, next }) => {
+  const matchHead = (html: string) => {
+    return html.match(/^<head>([\s\S]*)<\/head>$/i);
+  };
+
+  let cond = null;
+  let condValue = false;
+  let head = null;
+
+  if (node.condition) {
+    cond = createFunction(node.condition.expr);
+  }
+
+  try {
+    condValue = cond ? cond(context) : true;
+  } catch (error) {
+    condValue = false;
+  }
+
+  if (node.type === "html") {
+    let m = matchHead(node.value);
+    if (m) {
+      head = m[1];
+    }
+  }
+
+  return <>
+    {head && <div dangerouslySetInnerHTML={{ __html: head }}/>}
+    {condValue && 
+      
+    }
+    {node.type}
+  </>;
 };
 
 export default SurveyNode;
